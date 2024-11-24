e8daa509f727df2138e223daa5d8d47ecbbd8311
Glom Zhao <glom@skillw.com>
2024-11-24 15:41:07 +0800

    remove: software social [ wechat ]

--- software/social.nix
+++ software/social.nix
@@ -3,11 +3,11 @@
      fluffychat
      thunderbird
      qq
-     wechat-uos
   ];
   nixpkgs.config.permittedInsecurePackages = [
     "fluffychat-linux-1.20.0"
     "olm-3.2.16"
+    "openssl-1.1.1w"
   ];
 
 }

c4f5e25cfeef09fbbf3c9707ccdf92d3c255d986
Glom Zhao <glom@skillw.com>
2024-12-05 02:44:03 +0800

    fix: xdg home path

--- home/programs/xdg.nix
+++ home/programs/xdg.nix
@@ -5,11 +5,4 @@
     enable = true; # 启用 XDG 配置
     userDirs.enable = true; # 管理用户的常用目录
   };
-
-  # 自定义 XDG 目录
-  environment.variables = {
-    XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
-    XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
-    XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
-  };
 }

d9d3e5cbec519318cfdd271fdfa6c5ef63a0286d
Glom Zhao <glom@skillw.com>
2025-01-28 15:59:25 +0800

    new disk

--- hardware/zephyrus/disk.nix
+++ hardware/zephyrus/disk.nix
@@ -3,17 +3,18 @@
 {
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/f14ed014-2ad0-4255-ba95-0ff742103f80";
-      fsType = "ext4";
+    { device = "/dev/disk/by-uuid/5094e655-4d5d-48df-90db-45e62e42d1e1";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/92E3-6297";
+    { device = "/dev/disk/by-uuid/C3AB-ABB0";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/cca23fdf-2621-4793-bc5a-7c1ef9041e73"; }
+    [ { device = "/dev/disk/by-uuid/d7e33875-391a-4d29-9abe-4f990ef53fce"; }
     ];
 }
--- hardware/zephyrus/gpu/nvidia.nix
+++ hardware/zephyrus/gpu/nvidia.nix
@@ -19,7 +19,7 @@
     prime = {
       nvidiaBusId = "PCI:1:0:0";
     };
-    open = true;
+    open = false;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     nvidiaSettings = true;
@@ -35,32 +35,32 @@
     inherit (flake-inputs.nixpkgs-wayland.packages.${pkgs.system}) sway-unwrapped;
   };
 
-  boot = {
-    kernelParams = [ "nvidia-drm.fbdev=1" ];
+  # boot = {
+  #   kernelParams = [ "nvidia-drm.fbdev=1" ];
 
-    extraModprobeConfig =
-      "options nvidia "
-      + lib.concatStringsSep " " [
-        # nvidia assume that by default your CPU does not support PAT,
-        # but this is effectively never the case in 2023
-        "NVreg_UsePageAttributeTable=1"
-        # This is sometimes needed for ddc/ci support, see
-        # https://www.ddcutil.com/nvidia/
-        #
-        # Current monitor does not support it, but this is useful for
-        # the future
-        "NVreg_RegistryDwords=RMUseSwI2c=0x01;RMI2cSpeed=100"
-      ];
-  };
-
-  # environment.variables = {
-  #   GBM_BACKEND = "nvidia-drm";
-  #   # Apparently, without this nouveau may attempt to be used instead
-  #   # (despite it being blacklisted)
-  #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  #   # Hardware cursors are currently broken on nvidia
-  #   # TEST
-  #   WLR_NO_HARDWARE_CURSORS = "1";
+  #   extraModprobeConfig =
+  #     "options nvidia "
+  #     + lib.concatStringsSep " " [
+  #       # nvidia assume that by default your CPU does not support PAT,
+  #       # but this is effectively never the case in 2023
+  #       "NVreg_UsePageAttributeTable=1"
+  #       # This is sometimes needed for ddc/ci support, see
+  #       # https://www.ddcutil.com/nvidia/
+  #       #
+  #       # Current monitor does not support it, but this is useful for
+  #       # the future
+  #       "NVreg_RegistryDwords=RMUseSwI2c=0x01;RMI2cSpeed=100"
+  #     ];
   # };
+
+  #  environment.variables = {
+  #    GBM_BACKEND = "nvidia-drm";
+  #    # Apparently, without this nouveau may attempt to be used instead
+  #    # (despite it being blacklisted)
+  #    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  #    # Hardware cursors are currently broken on nvidia
+  #    # TEST
+  #    WLR_NO_HARDWARE_CURSORS = "1";
+  #  };
 }
 

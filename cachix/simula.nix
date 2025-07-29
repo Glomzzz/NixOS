2ded0e69e6ecc18ba0c50a5751e6d3e70f998c75
Glomzzz <glom@skillw.com>
2025-07-30 01:52:55 +0800

    simula cache

---
+++ cachix/simula.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://simula.cachix.org"
+      ];
+      trusted-public-keys = [
+        "simula.cachix.org-1:Sr0SD5FIjc8cUVIeBHl8VJswQEJOBIE6u3wpmjslGBA="
+      ];
+    };
+  };
+}

37630f6a77adb9414712e0078a15e1f1223d9052
Glomzzz <glom@skillw.com>
2025-08-12 17:24:24 +0800

    fix(r-a): failed to build

--- system/env.nix
+++ system/env.nix
@@ -1,11 +1,13 @@
-{pkgs,...} : {
-    environment.systemPackages = with pkgs; [
-      clang
-      # rustup to manage rust
-      # there will be errors if u install cargo & rustc with rustup here,
-      # due to some IDE will not recognize the rustc & cargo installed by rustup.
-      rustup
-      # java
-      openjdk
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    clang
+    # rustup to manage rust
+    # there will be errors if u install cargo & rustc with rustup here,
+    # due to some IDE will not recognize the rustc & cargo installed by rustup.
+    rustup
+
+    callPackage ./rust-analyzer-unwrapped/ { }
+    # java
+    openjdk
   ];
 }

9c5fc81a762ed6ddce27772d7a94460150d35a27
Glomzzz <glom@skillw.com>
2026-05-05 14:27:36 +0800

    fix(input): disable autosuspend for rog omni receiver

--- modules/nixos/core/input.nix
+++ modules/nixos/core/input.nix
@@ -15,13 +15,20 @@
     };
   };
 
+  # Keep the ROG Omni receiver awake. It is detected reliably at boot, but
+  # the device later disconnects and reconnects on its own.
+  services.udev.extraRules = ''
+    ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="0b05", ATTR{idProduct}=="1ace", TEST=="power/control", ATTR{power/control}="on"
+    ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="0b05", ATTR{idProduct}=="1ace", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}="-1"
+  '';
+
   # Additional input packages and tools
   environment.systemPackages = with pkgs; [
     libinput
     xf86-input-libinput
     # Diagnostic tools
     evtest
-    # Support for Logitech wireless devices
+    # Wireless receiver diagnostics
     solaar
   ];
 }

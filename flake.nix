d0023e971c84114abaa6b939216e7da584fd0750
Glomzzz <glom@skillw.com>
2026-05-16 14:12:55 +0800

    refactor(flake): remove emacs flake input

--- flake.nix
+++ flake.nix
@@ -25,88 +25,103 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixvim.inputs.nixpkgs.follows = "nixpkgs";
     };
-    emacs-config = {
-      url = "path:/home/glom/emacs-config";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # emacs-config = {
+    #   url = "path:/home/glom/emacs-config";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  }: let
-    systems = ["x86_64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    pkgsFor = system:
-      import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+    in
+    {
+      formatter = forAllSystems (system: (pkgsFor system).alejandra);
+
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          formatting =
+            pkgs.runCommand "alejandra-check"
+              {
+                nativeBuildInputs = [ pkgs.alejandra ];
+                src = ./.;
+              }
+              ''
+                cd "$src"
+                alejandra --check .
+                touch "$out"
+              '';
+
+          dead-code =
+            pkgs.runCommand "deadnix-check"
+              {
+                nativeBuildInputs = [ pkgs.deadnix ];
+                src = ./.;
+              }
+              ''
+                cd "$src"
+                deadnix --fail .
+                touch "$out"
+              '';
+
+          lint =
+            pkgs.runCommand "statix-check"
+              {
+                nativeBuildInputs = [ pkgs.statix ];
+                src = ./.;
+              }
+              ''
+                cd "$src"
+                statix check .
+                touch "$out"
+              '';
+
+          host = self.nixosConfigurations.nixos.config.system.build.toplevel;
+        }
+      );
+
+      nixosConfigurations = {
+        nixos =
+          let
+            system = "x86_64-linux";
+            hostname = "nixos";
+            username = "glom";
+            usernameFull = "Glom Zhao";
+            email = "glom@skillw.com";
+            specialArgs = {
+              inherit hostname;
+              inherit username;
+              inherit usernameFull;
+              inherit email;
+              inherit system;
+              inherit inputs;
+            };
+          in
+          nixpkgs.lib.nixosSystem {
+            inherit specialArgs;
+            inherit system;
+            modules = [
+              ./hosts/nixos
+              home-manager.nixosModules.home-manager
+            ];
+          };
       };
-  in {
-    formatter = forAllSystems (system: (pkgsFor system).alejandra);
-
-    checks = forAllSystems (system: let
-      pkgs = pkgsFor system;
-    in {
-      formatting =
-        pkgs.runCommand "alejandra-check" {
-          nativeBuildInputs = [pkgs.alejandra];
-          src = ./.;
-        } ''
-          cd "$src"
-          alejandra --check .
-          touch "$out"
-        '';
-
-      dead-code =
-        pkgs.runCommand "deadnix-check" {
-          nativeBuildInputs = [pkgs.deadnix];
-          src = ./.;
-        } ''
-          cd "$src"
-          deadnix --fail .
-          touch "$out"
-        '';
-
-      lint =
-        pkgs.runCommand "statix-check" {
-          nativeBuildInputs = [pkgs.statix];
-          src = ./.;
-        } ''
-          cd "$src"
-          statix check .
-          touch "$out"
-        '';
-
-      host = self.nixosConfigurations.nixos.config.system.build.toplevel;
-    });
-
-    nixosConfigurations = {
-      nixos = let
-        system = "x86_64-linux";
-        hostname = "nixos";
-        username = "glom";
-        usernameFull = "Glom Zhao";
-        email = "glom@skillw.com";
-        specialArgs = {
-          inherit hostname;
-          inherit username;
-          inherit usernameFull;
-          inherit email;
-          inherit system;
-          inherit inputs;
-        };
-      in
-        nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          inherit system;
-          modules = [
-            ./hosts/nixos
-            home-manager.nixosModules.home-manager
-          ];
-        };
     };
-  };
 }

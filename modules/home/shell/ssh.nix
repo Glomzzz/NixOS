4a1b450e9604b99207579a67388f7a48497a7e44
Glomzzz <glom@skillw.com>
2026-05-23 08:08:52 +0800

    add(ssh): add macmini host alias

--- modules/home/shell/ssh.nix
+++ modules/home/shell/ssh.nix
@@ -1,3 +1,12 @@
 {...}: {
-  programs.ssh.enable = true;
+  programs.ssh = {
+    enable = true;
+    settings = {
+      macmini = {
+        hostname = "mac-mini";
+        serverAliveInterval = 30;
+        serverAliveCountMax = 3;
+      };
+    };
+  };
 }

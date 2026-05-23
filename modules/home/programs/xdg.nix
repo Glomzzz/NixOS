0fa365f32a86826f25349344290184c18701f84e
Glomzzz <glom@skillw.com>
2026-05-23 08:16:23 +0800

    fix(emacs): prefer direct app for opened files

--- modules/home/programs/xdg.nix
+++ modules/home/programs/xdg.nix
@@ -8,20 +8,21 @@
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/plain" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-c" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-c++" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-c++hdr" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-c++src" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-chdr" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-csrc" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-java" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-makefile" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-moc" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-pascal" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-tcl" = ["emacsclient.desktop" "emacs.desktop"];
-        "text/x-tex" = ["emacsclient.desktop" "emacs.desktop"];
-        "application/x-shellscript" = ["emacsclient.desktop" "emacs.desktop"];
+        "application/json" = ["emacs.desktop" "emacsclient.desktop"];
+        "application/x-shellscript" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/plain" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-c" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-c++" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-c++hdr" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-c++src" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-chdr" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-csrc" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-java" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-makefile" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-moc" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-pascal" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-tcl" = ["emacs.desktop" "emacsclient.desktop"];
+        "text/x-tex" = ["emacs.desktop" "emacsclient.desktop"];
         "x-scheme-handler/org-protocol" = ["emacsclient.desktop"];
       };
     };

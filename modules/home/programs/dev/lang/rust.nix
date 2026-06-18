67a60d8fed34c10f62c70f2e305faa9ae29c09b8
Glomzzz <glom@skillw.com>
2026-06-18 10:43:56 +0800

    fix(cargo build): missing OPENSSL

--- modules/home/programs/dev/lang/rust.nix
+++ modules/home/programs/dev/lang/rust.nix
@@ -7,4 +7,11 @@
     openssl
     openssl.dev
   ];
+
+  home.sessionVariables = {
+    OPENSSL_DIR = "${pkgs.openssl.dev}";
+    OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+    OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  };
 }

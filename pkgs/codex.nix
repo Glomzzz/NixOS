77cc883e435b8345abfded71067aafb5ea4689f4
Glomzzz <glom@skillw.com>
2026-06-18 10:49:17 +0800

    update(codex): codex 0.140.0

--- pkgs/codex.nix
+++ pkgs/codex.nix
@@ -11,11 +11,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "codex";
-  version = "0.133.0";
+  version = "0.140.0";
 
   src = fetchurl {
     url = "https://github.com/openai/codex/releases/download/rust-v${finalAttrs.version}/codex-x86_64-unknown-linux-musl.tar.gz";
-    hash = "sha256-0GAZq5w10oG3jcLrsq5VwruX6hG/f0Urr+OQ7dsANO8=";
+    hash = "sha256-SOWRlk1Luqt8BDPa7UHGXqjxUvwPHdIzSkLj3TIqSQY=";
   };
 
   sourceRoot = ".";

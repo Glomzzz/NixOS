a85ac08636169ac509a6531bd66466cb3f81a386
Glomzzz <glom@skillw.com>
2026-08-05 16:14:27 +0800

    fix(audio): start EasyEffects after graphical session

--- modules/home/programs/media.nix
+++ modules/home/programs/media.nix
@@ -15,14 +15,17 @@
     Unit = {
       Description = "EasyEffects audio processor";
       After = [
+        "graphical-session.target"
         "pipewire.service"
         "wireplumber.service"
       ];
+      ConditionEnvironment = "WAYLAND_DISPLAY";
       PartOf = ["graphical-session.target"];
     };
     Service = {
       ExecStart = "${pkgs.easyeffects}/bin/easyeffects --service-mode";
       Restart = "on-failure";
+      RestartSec = 3;
     };
     Install.WantedBy = ["graphical-session.target"];
   };

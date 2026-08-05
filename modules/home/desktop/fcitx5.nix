0cfdbb3aa03dd7a4bfd695ed68e51237bbffa93c
Glomzzz <glom@skillw.com>
2026-08-06 05:31:19 +0800

    fix(input): enlarge Fcitx interface

--- modules/home/desktop/fcitx5.nix
+++ modules/home/desktop/fcitx5.nix
@@ -19,6 +19,46 @@
 
   xdg = {
     configFile = {
+      "fcitx5/conf/classicui.conf" = {
+        force = true;
+        text = ''
+          # Vertical Candidate List
+          Vertical Candidate List=False
+          # Use mouse wheel to go to prev or next page
+          WheelForPaging=True
+          # Font
+          Font="Sans 12"
+          # Menu Font
+          MenuFont="Sans 12"
+          # Tray Font
+          TrayFont="Sans Bold 12"
+          # Tray Label Outline Color
+          TrayOutlineColor=#000000
+          # Tray Label Text Color
+          TrayTextColor=#ffffff
+          # Prefer Text Icon
+          PreferTextIcon=False
+          # Show Layout Name In Icon
+          ShowLayoutNameInIcon=True
+          # Use input method language to display text
+          UseInputMethodLanguageToDisplayText=True
+          # Theme
+          Theme=plasma
+          # Dark Theme
+          DarkTheme=plasma
+          # Follow system light/dark color scheme
+          UseDarkTheme=False
+          # Follow system accent color if it is supported by theme and desktop
+          UseAccentColor=True
+          # Use Per Screen DPI on X11
+          PerScreenDPI=False
+          # Keep the base DPI neutral so Wayland can apply each output's scale.
+          ForceWaylandDPI=0
+          # Enable fractional scale under Wayland
+          EnableFractionalScale=True
+        '';
+      };
+
       "fcitx5/profile" = {
         force = true;
         text = ''

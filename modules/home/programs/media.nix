a5de52b6470fc619a9eb39d6a2dfb64af66dd376
Glomzzz <glom@skillw.com>
2026-08-08 01:35:44 +0800

    fix(easyeffects): no window mode

--- modules/home/programs/media.nix
+++ modules/home/programs/media.nix
@@ -21,7 +21,7 @@
       PartOf = ["graphical-session.target"];
     };
     Service = {
-      ExecStart = "${pkgs.easyeffects}/bin/easyeffects --service-mode";
+      ExecStart = "${pkgs.easyeffects}/bin/easyeffects --service-mode --hide-window";
       Restart = "on-failure";
     };
     Install.WantedBy = ["graphical-session.target"];

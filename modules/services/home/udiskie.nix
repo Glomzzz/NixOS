83de65154401723a168f3aa4a1c9409cac5d1d80
Glomzzz <glom@skillw.com>
2026-08-08 01:31:06 +0800

    fix(udiskie): test failed

--- modules/services/home/udiskie.nix
+++ modules/services/home/udiskie.nix
@@ -1,4 +1,11 @@
-_: {
+{pkgs, ...}: {
   # auto mount usb drives
-  services.udiskie.enable = true;
+  services.udiskie = {
+    enable = true;
+    package = pkgs.udiskie.overridePythonAttrs (oldAttrs: {
+      disabledTestPaths =
+        (oldAttrs.disabledTestPaths or [])
+        ++ ["test/test_keyutils.py::TestKeyutils::test_invalidate"];
+    });
+  };
 }
